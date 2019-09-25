210406d6ec04ee15326a128455a45d7fc4122b23
phatdroid94 <phatdroid94@gmail.com>
2019-09-25 20:37:43 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/gameloto04092019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/gameloto04092019/MainActivity.java
@@ -3,12 +3,37 @@ package phamtanphat.ptp.khoaphamtraining.gameloto04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnRandom;
+    TextView txtResult;
+    EditText edtMin,edtMax;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 1 : Su dung alt + enter goi y code
+        initView();
+
+        // rule , graph
+        //Task check valid number:
+        // 1 : Phai nhap gia tri cho 2 edittext
+        // 2 : Neu so min ma lon max : max = min + 1
+        // 3 : So min khong duoc < 0
+        //Task handle random:
+        // Cac so random khong duoc lap lai
+        //Task show toast
+        // loi hay thanh cong thong bao cho nguoi dung biet
+    }
+
+    private void initView() {
+        btnRandom = findViewById(R.id.buttonRandom);
+        txtResult = findViewById(R.id.textviewResult);
+        edtMin = findViewById(R.id.edittextMin);
+        edtMax = findViewById(R.id.edittextMax);
     }
 }
